be640ac827a4c7b424f9119b13330af3ac099949
priyanshu gupta <114559727+void0205@users.noreply.github.com>
2022-12-06 19:07:45 +0530

    Add files via upload

---
+++ L1 comparison python.py
@@ -0,0 +1,41 @@
+x=9
+print("Assignment operator is:", x)
+x+=2
+print("+= results in:", x)
+x-=2
+print("-= results in:", x)
+x*2
+print("*=results in:", x)
+x/=2
+print("/= results in:", x)
+x//=2
+print("//=results in:", x)
+x%=2
+print("%=results in:", x)
+x**2
+print("**=results in:", x)
+
+
+a, b = 18,5
+print("a Equals b:", a==b)
+print("a NOT Equal to b:", a!=b)
+print("a>b:", a>b)
+print("a<b:", a<b)
+print("a>=b", a>=b)
+print("a<=b", a<=b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
